e382c690ae3a34d59e9543fe132006c0fa170ff4
Vinicius <vinioliveira015@gmail.com>
2020-09-26 00:07:00 -0300

    This java class tests the previously created class Cliente.

---
+++ aj2lab01_02/src/main/java/TestaCliente.java
@@ -0,0 +1,21 @@
+/*
+ * Globalcode - "The Developers Company"
+ * 
+ * Academia do Java
+ */
+class TestaCliente {
+
+    public static void main(String[] args) {
+        // Criacao do cliente
+        // Inicializacao do cliente
+        // Impressao dos dados do cliente
+    	
+
+    	Cliente ze = new Cliente();
+    		
+    ze.inicializaCliente("Zezin", "425852585");
+    
+    ze.imprimeDados();
+    
+    }   
+}
