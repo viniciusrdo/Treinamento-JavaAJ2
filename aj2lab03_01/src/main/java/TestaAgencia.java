3f24778fc683d6d9fe979179b3cc211ba0519d42
Vinicius <vinioliveira015@gmail.com>
2020-09-26 01:04:28 -0300

    This project is a representation of a bank account, using some JAVA resources

---
+++ aj2lab03_01/src/main/java/TestaAgencia.java
@@ -0,0 +1,20 @@
+/*
+ * Globalcode - "The Developers Company"
+ * 
+ * Academia do Java
+ */
+public class TestaAgencia {
+
+    public static void main(String[] args) {
+        // Inicializar um objeto do tipo Agencia
+        // usar o metodo inicializaAgencia (ver quais sao os parametros na classe Agencia)
+        // Imprimir os dados da agencia
+    	
+    	Agencia ag1 = new Agencia();
+    	
+    	ag1.inicializaAgencia("14525", 556);
+    	
+    	ag1.imprimeDados();
+    	
+    }
+}
