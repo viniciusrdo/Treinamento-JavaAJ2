71839fbce198f043e4858b917c4b03331967398d
Vinicius <vinioliveira015@gmail.com>
2020-09-26 00:06:16 -0300

    this is a java class that testes the Agencia class, previously created.

---
+++ aj2lab01_02/src/main/java/TestaAgencia.java
@@ -0,0 +1,20 @@
+/*
+ * Globalcode - "The Developers Company"
+ * 
+ * Academia do Java
+ */
+class TestaAgencia {
+
+    public static void main(String[] args) {
+        // Criacao da agencia
+        // Inicializacao da agencia
+        // Impressao dos dados da agencia
+    	
+    	Agencia ag = new Agencia();
+    	
+    	ag.inicializaAgencia("15136", 13);
+    	
+    	ag.imprimeDados();
+    	
+    }
+}
