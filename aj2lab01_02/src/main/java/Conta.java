b16ea541de858e5a3ab4e2835e20d64c692b8d51
Vinicius <vinioliveira015@gmail.com>
2020-09-26 00:05:04 -0300

    This is a java class that represent a bank account and its attributes

---
+++ aj2lab01_02/src/main/java/Conta.java
@@ -0,0 +1,97 @@
+/*
+ * Globalcode - "The Developers Company"
+ * 
+ * Academia do Java
+ * O metodo inicializaConta e util para evitar a necessidade de inicializar
+ * atributo por atributo, desta forma podemos chamar um unico metodo e passar
+ * todos os parametros em uma unica chamada.
+ * 
+ * 1) Implemente os metodos que nao foram implementados na classe Conta de 
+ * acordo com a especificacao nos metodos.
+ */
+class Conta {
+
+    double saldo;
+    String numero;
+    String titular;
+    int agencia;
+    int banco;
+
+    /**
+     * @param saldoInicial
+     *            Saldo Inicial da conta
+     * @param num
+     *            Numero da conta
+     * @param tit
+     *            Titular da conta
+     * @param ag
+     *            Agencia a qual a conta pertence
+     * @param bc
+     *            Banco a qual a agencia pertence
+     */
+    void inicializaConta(double saldoInicial, String num, String tit, int ag, int bc) {
+        System.out.println("Inicializando uma conta com os seguintes dados:");
+        saldo = saldoInicial;
+        numero = num;
+        titular = tit;
+        agencia = ag;
+        banco = bc;
+    }
+
+    /**
+     * @param valor:
+     *            valor a ser sacado da conta
+     */
+    /*
+     * 1. Verificar se o valor do saque e positivo.
+     * 2. Verificar se ha saldo suficiente para efetuar o saque 
+     *    2.1. Se o saldo for suficiente, efetuar o saque 
+     *    2.2. Se o saldo for insuficiente imprimir na tela que o saldo e insuficiente
+     */
+    void saque(double valor) {
+    	if (valor <= saldo) {
+    		saldo = saldo - valor;
+    	}
+    	else {
+    		System.out.println("Saldo insulficiente!!");
+    	}
+    
+    }
+
+    /**
+     * @param valor
+     *            Valor a ser depositado da conta
+     */
+    /*
+     * 1. Verificar se o valor do deposito e positivo, se for efetue o deposito
+     */
+    void deposito(double valor) {
+    	
+    	if(valor>0) {
+    		saldo = saldo + valor;
+    	}
+    	else {
+    		System.out.println("Deposito invalido, por favor verifique e tente novamente!!");
+    	}
+    	
+    }
+
+    /**
+     * Metodo para impressao de todos os dados da classe
+     */
+    void imprimeDados() {
+        System.out.println("\n----------------------------");
+        System.out.println("AGENCIA:\t" + agencia + "\t BANCO:\t" + banco);
+        System.out.println("NUMERO: \t" + numero);
+        System.out.println("TITULAR: \t" + titular);
+        System.out.println("SALDO: \t" + saldo);
+        System.out.println("-----------------------------\n");
+    }
+
+    /**
+     * @return saldo da conta
+     */
+    double getSaldo() {
+        return saldo;
+    }
+}
