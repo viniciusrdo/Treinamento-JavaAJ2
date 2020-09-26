5ebabfe952e881e7001d35a556f2cf5d223cb897
Vinicius <vinioliveira015@gmail.com>
2020-09-26 01:04:42 -0300

    This project is a representation of a bank account, using some JAVA resources

---
+++ aj2lab03_01/src/main/java/TestaCliente.java
@@ -0,0 +1,23 @@
+/*
+ * Globalcode - "The Developers Company"
+ * 
+ * Academia do Java
+ */
+/**
+ * @author Globalcode
+ * 
+ */
+public class TestaCliente {
+
+    public static void main(String[] args) {
+        // Criacao do cliente
+        // Inicializacao do cliente usando o metodo inicializaCliente
+        // Impressao dos dados do cliente
+    	
+    	Cliente ze = new Cliente();
+    	
+    	ze.inicializaCliente("Ze", "41525852414");
+
+    	ze.imprimeDados();
+    }
+}
