acc919bf2a28a8cc46944297e0c66ada1c131190
Vinicius <vinioliveira015@gmail.com>
2020-09-25 23:24:59 -0300

    this is an java class that represente a costumer of a bank and its atributes

---
+++ aj2lab01_01/src/main/java/Cliente.java
@@ -0,0 +1,15 @@
+/*
+ * Globalcode - "The Developers Company"
+ * 
+ * Academia do Java
+ * 
+ * 1) Adicione os seguintes atributos na classe Cliente: 
+ * - nome (String) 
+ * - cpf (String)
+ */
+class Cliente {
+	
+	String nome;
+	
+	String cpf;
+}
