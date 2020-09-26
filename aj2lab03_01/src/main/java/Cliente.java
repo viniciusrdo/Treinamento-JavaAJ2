7fbac2a50edf06ac62008de3cd66cb5f00c83173
Vinicius <vinioliveira015@gmail.com>
2020-09-26 01:04:00 -0300

    This project is a representation of a bank account, using some JAVA resources

---
+++ aj2lab03_01/src/main/java/Cliente.java
@@ -0,0 +1,50 @@
+/*
+ * Globalcode - "The Developers Company"
+ * 
+ * Academia do Java
+ * 
+ * 1) Encapsule os atributos da classe Cliente
+ * 
+ */
+public class Cliente {
+
+   private String nome;
+   private String cpf;
+
+    /**
+     * @param nomeCliente
+     *            nome do cliente
+     * @param cpfCliente
+     *            cpf do cliente
+     */
+    public void inicializaCliente(String nomeCliente, String cpfCliente) {
+        this.cpf = cpfCliente;
+        this.nome = nomeCliente;
+    }
+
+    public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	/**
+     * Metodo para impressao de todos os dados da classe
+     */
+    void imprimeDados() {
+        System.out.println("----------------------");
+        System.out.println("Nome do cliente :" + this.nome);
+        System.out.println("CPF:" + this.cpf);
+        System.out.println("----------------------");
+    }
+}
