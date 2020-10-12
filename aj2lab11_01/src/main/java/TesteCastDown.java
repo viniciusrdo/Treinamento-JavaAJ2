b74e597d3bab056a0d490be1a256b345d55237b6
Vinicius <vinioliveira015@gmail.com>
2020-10-12 17:48:51 -0300

    Este eh um exeplo da aplicacao de polimorfismo em Java

---
+++ aj2lab11_01/src/main/java/TesteCastDown.java
@@ -0,0 +1,35 @@
+public class TesteCastDown {
+
+    public static void main(String[] args) {
+
+        double saldoInicial = args.length > 0 ? Double.parseDouble(args[0]) : 0d;
+        
+        Banco banco = new Banco("GlobalcodeBank", 123);
+        Agencia ag = new AgenciaStandard("9999", banco);
+        Cliente clienteW = new Cliente("Mr. W", "55446688-77");
+        Conta conta = ag.abrirConta(clienteW, saldoInicial);
+        
+        System.out.println("Foi criada a conta " + conta.getNumero() + 
+                           " para " + conta.getTitular().getNome() + 
+                           " com saldo de " + conta.getSaldo());
+        
+        // Aqui estamos investigando se a conta aberta é do tipo poupanca
+        if (conta instanceof ContaPoupanca) {
+            
+            System.out.println("Trata-se de uma conta poupanca");
+            ContaPoupanca contaPoupanca = (ContaPoupanca) conta; // cast down para visualizar o aniversario
+            System.out.println("Aniversario: " + contaPoupanca.getDataAniversario());
+            
+        } else if (false) { // Investigue se a conta aberta é do tipo especial
+            
+            // em caso afirmativo imprima uma mensagen de aviso, e o limite estabelecido
+            
+        } else {
+            
+            // em caso negativo imprima uma mensagem afirmando que é uma conta simples
+            
+        }
+        
+        
+    }
+}
